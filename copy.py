bdb3346a4eaee9be5082856b5f916c8a3194ad15
LiKaiwen <lkw14@mails.tsinghua.edu.cn>
2016-08-17 20:31:14 +0800

    add copy.py to copy the files in other folders

---
+++ copy.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import os
+
+os.system("copy ..\\Step1_create_input\\step1_parameters_all_platform_water_layer.py .")
+os.system("copy ..\\Step2_read_output\\step2_read_output_data.py .")
+os.system("copy ..\\Step3_compare_and_plot\\step3_compare_and_plot_no_scatter.py .")
